ac33aef7b6b8b32e850c4da3b167d81a5814410c
Brandon Keepers <brandon@opensoul.org>
2014-08-22 11:26:56 -0400

    Docs for Variable

--- lib/envy/variable.rb
+++ lib/envy/variable.rb
@@ -2,40 +2,72 @@ module Envy
   class Variable
     attr_reader :name, :options
 
+    # name               - the name of the environment variable
+    # options[:default]  - a default value or Proc if the variable is not set
+    # options[:required] - a boolean indiciting if a value is required
     def initialize(name, options = {}, &default)
       @name = name
       @options = options
       @default = default || options[:default]
     end
 
-    def required?(context)
+    # Determine if this variable is required.
+    #
+    # If `options[:required]` is a Proc or symbol, it will be instance evaled
+    # in the environment.
+    #
+    #     desc "Should SSL be required?"
+    #     boolean :force_ssl, :default => true
+    #
+    #     desc "PEM file with encryption keys"
+    #     string :pem, :required => :force_ssl?
+    #
+    # environment - an instance of Envy::Environment
+    #
+    # Returns true if the variable is required.
+    def required?(environment)
       if options[:required].respond_to?(:to_proc)
-        context.instance_eval(&options[:required])
+        environment.instance_eval(&options[:required])
       else
         !!options[:required]
       end
     end
 
+    # The name of the accessor method that will be defined on the
+    # `Envy::Environment` instance. Override in subclasses to customize.
     def accessor_name
       name
     end
 
-    def accessor(context)
-      cast(value(context)).tap do |result|
-        raise ArgumentError, "#{name} is required" if required?(context) && result.nil?
+    # The method that gets defined on the environment to read this variable.
+    #
+    # environment - an instance of Envy::Environment
+    #
+    # Returns the cast environment variable.
+    def accessor(environment)
+      cast(value(environment)).tap do |result|
+        raise ArgumentError, "#{name} is required" if required?(environment) && result.nil?
       end
     end
 
-    def value(context)
-      context.env.fetch(name.to_s.upcase) do
+    # Fetch the value from the environment
+    #
+    # environment - an instance of Envy::Environment
+    #
+    # Returns a string from the environment variable, or the default value.
+    def value(environment)
+      environment.env.fetch(name.to_s.upcase) do
         if @default.respond_to?(:to_proc)
-          context.instance_eval(&@default)
+          environment.instance_eval(&@default)
         else
           @default
         end
       end
     end
 
+    # Override in subclasses to perform casting.
+    #
+    # value - A string from the environment, or the default value
     def cast(value)
       value
     end
