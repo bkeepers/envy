ddf7c4ab994fcd8629774b9c922e2365349c4748
Brandon Keepers <brandon@opensoul.org>
2014-08-27 18:16:58 -0400

    Rescue error in rails unless Envfile is explicitly set

--- lib/envy/railtie.rb
+++ lib/envy/railtie.rb
@@ -1,9 +1,16 @@
 module Envy
   class Railtie < Rails::Railtie
+    def envfile
+      ENV["ENVFILE"] || Rails.root.join('Envfile')
+    end
+
     config.before_configuration do |app|
-      app.extend(Envy)
-      envfile = ENV["ENVFILE"] || Rails.root.join('Envfile')
-      Envy.env = app.env = Envy::Environment.new(ENV).configure(envfile)
+      begin
+        Envy.env = Envy::Environment.new(ENV).configure(envfile)
+      rescue Envy::EnvfileNotFound => e
+        # re-raise if ENVFILE is explicitly defined.
+        raise if ENV["ENVFILE"]
+      end
     end
 
     rake_tasks do
--- spec/envy/railtie_spec.rb
+++ spec/envy/railtie_spec.rb
@@ -4,30 +4,48 @@ require "envy/railtie"
 require "rake"
 
 describe Envy::Railtie do
-  let(:envfile) { fixture_path("Envfile") }
   let(:application) { Class.new(Rails::Application) }
 
   before do
     @env = ENV.to_h
     Rails.application = nil
-    ENV["ENVFILE"] = envfile
   end
 
   after do
-    ENV.update @env
+    ENV.replace @env
     Envy.env = nil
-  end
-
-  it "defines #env on the application" do
-    expect(application.env).to be(Envy.env)
-  end
-
-  it "evalates the Envfile" do
-    expect(application.env).to respond_to(:from_envfile)
   end
 
   it "defines rake tasks" do
     application.load_tasks
     expect(Rake.application["env"]).to be_instance_of(Rake::Task)
+  end
+
+  context "when Envfile exists" do
+    it "evalates the Envfile" do
+      # Rails uses existance of config.ru and falls back to Dir.pwd to set Rails.root
+      Dir.chdir(fixture_path) { application }
+      expect(Envy.env).to respond_to(:from_envfile)
+    end
+  end
+
+  context "with ENV['ENVFILE']" do
+    it "evalates the Envfile" do
+      ENV["ENVFILE"] = fixture_path("Envfile")
+      application
+      expect(Envy.env).to respond_to(:from_envfile)
+    end
+
+    it "raises an error if does not exist" do
+      ENV["ENVFILE"] = fixture_path("nope")
+      expect { application }.to raise_error(Envy::EnvfileNotFound)
+    end
+  end
+
+  context "when Envfile does not exist" do
+    it "does not raise error" do
+      expect(File.exists?("#{Dir.pwd}/Envfile")).to be(false)
+      expect { application }.not_to raise_error
+    end
   end
 end
