4855a2103323f760efda1d8d1d8c613a2d850994
Brandon Keepers <brandon@opensoul.org>
2014-08-28 23:18:10 -0400

    Doc the Railtie

--- lib/envy/railtie.rb
+++ lib/envy/railtie.rb
@@ -1,4 +1,11 @@
 module Envy
+  # Rails integration to automatically load `Envfile` and define the rake task.
+  #
+  # `Envfile` will automatically get loaded from the root of the Rails app when
+  # the application boots. An alternate Envfile can be loaded by setting
+  # `ENV["ENVFILE"]`.
+  #
+  # The `rake env` task also gets loaded, which will list out the Envy config.
   class Railtie < Rails::Railtie
     config.before_configuration do |app|
       envfile = ENV["ENVFILE"] || Rails.root.join('Envfile')
