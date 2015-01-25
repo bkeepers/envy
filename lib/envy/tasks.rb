1f0303058f36987153afd189ef4bf5ae49d70016
Brandon Keepers <brandon@opensoul.org>
2015-01-25 13:27:54 -0500

    Print all declared variables

--- lib/envy/tasks.rb
+++ lib/envy/tasks.rb
@@ -2,9 +2,8 @@ desc "Show environment variables that the application uses."
 task :env do
   Envy.env.each do |variable|
     description = variable.options[:description]
-    next unless description
 
-    puts description.gsub(/^/m, "# ")
+    puts description.gsub(/^/m, "# ") if description
     puts "# #{variable.from}=#{variable.default}"
     puts
   end
